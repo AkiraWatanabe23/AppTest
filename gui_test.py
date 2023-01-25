7ace671ba0460c48890c666c37e09559012da2c3
Akira Watanabe <a0323w@gmail.com>
2023-01-25 18:20:43 +0900

    入力ボックスの作成

--- gui_test.py
+++ gui_test.py
@@ -6,5 +6,14 @@ root = tkinter.Tk()
 root.title("Test App")
 root.geometry("500x500")
 
+#ラベル
+test_label = tkinter.Label(text='test', foreground='#0000ff')
+test_label.pack()
+
+#入力ボックス
+entry_box = tkinter.Entry(width=50)
+entry_box.insert(tkinter.END, "ここに文字列入れてね")
+entry_box.pack()
+
 #ウィンドウ状態の維持
 root.mainloop()
